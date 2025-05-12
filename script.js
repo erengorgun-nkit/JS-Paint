dcb026a07cd5302e049bd005875aedd9d65459a7
erengorgun-nkit <115351981+erengorgun-nkit@users.noreply.github.com>
2025-05-13 00:17:14 +0300

    brush and eraser works, range crash fix

--- script.js
+++ script.js
@@ -6,13 +6,15 @@ const btnShading = document.querySelector("#btn-shading");
 const btnLighten = document.querySelector("#btn-lighten");
 const btnEyeDropper = document.querySelector("#btn-eyedropper");
 const rangeCanvasSize = document.querySelector("#canvas-size");
-const txtRangeValue = document.querySelector("#range-value");
+const btnRangeValue = document.querySelector("#range-value");
 const btnClear = document.querySelector("#btn-clear");
 const btnExport = document.querySelector("#btn-export");
 const btnGrid = document.querySelector("#grid-toggle");
 const canvas = document.querySelector(".canvas");
+const tools = document.querySelector(".tools");
 
 newCanvas();
+btnRangeValue.textContent = `${rangeCanvasSize.value}x${rangeCanvasSize.value}`;
 
 // generate new canvas
 function newCanvas() {
@@ -21,27 +23,28 @@ function newCanvas() {
         let pixel = document.createElement("div");
         pixel.classList.add("pixel");
         pixel.style.width = `${100 / rangeCanvasSize.value}%`;
-        pixel.style.border = "1px solid rgb(144, 159, 173)";
         canvas.appendChild(pixel);
         changeBgColor();
     }
-    txtRangeValue.textContent =
-        `${rangeCanvasSize.value}x${rangeCanvasSize.value}`;
 }
+
+rangeCanvasSize.addEventListener("input", () => {
+    btnRangeValue.textContent = `${rangeCanvasSize.value}x${rangeCanvasSize.value}`
+});
 
 inputBgColor.addEventListener("input", () => {
     changeBgColor();
 });
 
 // set canvas size
-rangeCanvasSize.addEventListener("input", () => {
+btnRangeValue.addEventListener("click", () => {
     newCanvas();
 });
 
 // toogle grid lines
-let gridState = true;
+let gridState = false;
 btnGrid.addEventListener("click", () => {
-    if (gridState == true) {
+    if (gridState === true) {
         for (let i = 0; i < canvas.children.length; i++) {
             canvas.children[i].style.border = "none";
         }
@@ -80,4 +83,69 @@ btnExport.addEventListener("click", () => {
         link.href = newCanvas.toDataURL();
         link.click();
     });
+});
+
+
+
+
+// resets all buttons
+function toolStateReset() {
+    for (let i = 0; i < tools.children.length; i++) {
+        let tool = tools.children[i];
+        if (tool.tagName === "BUTTON" && tool.classList.contains("on")) {
+            tool.classList.remove("on");
+        }
+    }
+}
+
+
+// handles all individual tool toggles
+for (let i = 0; i < tools.children.length; i++) {
+    let btn = tools.children[i];
+    if (btn.tagName === "BUTTON") {
+        btn.addEventListener("click", () => {
+            if (btn.classList.contains("on")) {
+                btn.classList.remove("on");
+                console.log(`${btn.textContent} off`);
+            } else {
+                toolStateReset();
+                btn.classList.add("on");
+                console.log(`${btn.textContent} on`);
+            }
+        });
+    }
+}
+
+
+
+
+let isPainting = false;
+
+canvas.addEventListener("click", (event) => {
+    // eyedropper state
+    if (btnEyeDropper.classList.contains("on")) {
+        isPainting = false;
+        inputColor.value = event.target.style.backgroundColor;
+    }
+});
+canvas.addEventListener("mousedown", () => {
+    isPainting = true;
+});
+canvas.addEventListener("mouseover",  (event) => {
+    let target = event.target;
+    if (isPainting === true) {
+        // eraser state
+        if (btnEraser.classList.contains("on")) {
+            target.style.backgroundColor = inputBgColor.value;
+            target.removeAttribute("id", "brushed");
+        }
+        // brush state
+        else{
+            target.style.backgroundColor = inputColor.value;
+            target.setAttribute("id", "brushed");
+        }
+    }
+});
+canvas.addEventListener("mouseup", () => {
+    isPainting = false;
 });
